cb04d25be0b55f582d8ee81e4c8bcdb9d0c94f27
chenan <chenan2005@gmail.com>
2015-06-12 00:54:16 +0800

    add debugdll and releasedll compile option

--- dist.bat
+++ dist.bat
@@ -1,20 +1,39 @@
 @echo off
 if exist lua-5.3.0 rd /s /q lua-5.3.0
 md lua-5.3.0
+md lua-5.3.0\debug
+md lua-5.3.0\release
+md lua-5.3.0\debugdll
+md lua-5.3.0\releasedll
 rem lauxlib.h lua.h lua.hpp luaconf.h lualib.h
 copy src\lauxlib.h lua-5.3.0\
 copy src\lua.h lua-5.3.0\
 copy src\lua.hpp lua-5.3.0\
 copy src\luaconf.h lua-5.3.0\
 copy src\lualib.h lua-5.3.0\
-copy debug\lualib.lib lua-5.3.0\lualib_d.lib
-copy debug\luac.exe lua-5.3.0\luac_d.exe
-copy debug\luac.pdb lua-5.3.0\luac_d.pdb
-copy debug\lua.exe lua-5.3.0\lua_d.exe
-copy debug\lua.pdb lua-5.3.0\lua_d.pdb
-copy release\lualib.lib lua-5.3.0\
-copy release\luac.exe lua-5.3.0\
-copy release\luac.pdb lua-5.3.0\
-copy release\lua.exe lua-5.3.0\
-copy release\lua.pdb lua-5.3.0\
+copy debug\lualib.lib lua-5.3.0\debug\
+copy debug\luac.exe lua-5.3.0\debug\
+copy debug\luac.pdb lua-5.3.0\debug\
+copy debug\lua.exe lua-5.3.0\debug\
+copy debug\lua.pdb lua-5.3.0\debug\
+copy debug\vc80.pdb lua-5.3.0\debug\
+copy release\lualib.lib lua-5.3.0\release\
+copy release\luac.exe lua-5.3.0\release\
+copy release\luac.pdb lua-5.3.0\release\
+copy release\lua.exe lua-5.3.0\release\
+copy release\lua.pdb lua-5.3.0\release\
+copy release\vc80.pdb lua-5.3.0\release\
+
+copy debugdll\lualib.lib lua-5.3.0\debugdll\
+copy debugdll\lualib.dll lua-5.3.0\debugdll\
+copy debugdll\lua.exe lua-5.3.0\debugdll\
+copy debugdll\lua.pdb lua-5.3.0\debugdll\
+copy debugdll\vc80.pdb lua-5.3.0\debugdll\
+
+copy debugdll\lualib.lib lua-5.3.0\debugdll\
+copy debugdll\lualib.dll lua-5.3.0\debugdll\
+copy debugdll\lua.exe lua-5.3.0\debugdll\
+copy debugdll\lua.pdb lua-5.3.0\debugdll\
+copy debugdll\vc80.pdb lua-5.3.0\debugdll\
+
 pause
